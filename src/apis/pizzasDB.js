2690f4cc82b2056dc51b9e493609c4cc035fd7b1
findLogic <66243299+findLogic@users.noreply.github.com>
2020-06-09 14:20:07 +0300

    Update pizzasDB.js

--- src/apis/pizzasDB.js
+++ src/apis/pizzasDB.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://server-we-love-pizza.netlify.app:5000',
+  baseURL: 'https://server-we-love-pizza.herokuapp.com:5000',
   // baseURL: 'http://localhost:3333',
 });
