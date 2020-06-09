cbe31833f5993176486e86a168ff40847630ba8c
findLogic <66243299+findLogic@users.noreply.github.com>
2020-06-09 13:21:18 +0300

    Add files via upload

--- src/apis/pizzasDB.js
+++ src/apis/pizzasDB.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://optimistic-spence-6b2572.netlify.app:3333',
+  baseURL: 'https://optimistic-spence-6b2572.netlify.app',
   // baseURL: 'http://localhost:3333',
 });
